808b2df8644c810ec7eb16b3f8a69d2607e29804
h2roverpete <pete@h2rover.com>
2025-11-21 07:52:53 -0800

    Move breadcrumb data to Page component. Add better "active" states to navbar elements. Add breadcrumb docs.

--- ui/content/Breadcrumbs.js
+++ ui/content/Breadcrumbs.js
@@ -2,43 +2,36 @@ import {useContext} from "react";
 import {PageContext} from "./Page";
 import {SiteContext} from "./Site";
 
+/**
+ * @typedef BreadcrumbProps
+ * @property {string} [delimiter]   Delimiter to use between pages.
+ */
+/**
+ * Display breadcrumb trail in site navigation.
+ *
+ * @param props {BreadcrumbProps}
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function Breadcrumbs(props) {
 
-  const {outlineData, siteData} = useContext(SiteContext);
-  const {pageData} = useContext(PageContext);
-  const breadcrumbs = buildBreadcrumbs(outlineData, pageData?.ParentID);
+  const {siteData} = useContext(SiteContext);
+  const {breadcrumbs} = useContext(PageContext);
 
-  return (<>{breadcrumbs.length > 0 && (
+  return (<>{breadcrumbs?.length > 0 && (
     <div className="Breadcrumbs">
       {siteData?.SiteName}
       <>&nbsp;&raquo;&nbsp;</>
       {breadcrumbs.map(page => (
         <span key={page.PageID}>
           {page.PageTitle}
-          <>&nbsp;&raquo;&nbsp;</>
+          <>{props.delimiter ? (
+            <>{props.delimiter}</>
+          ) : (
+            <>&nbsp;&raquo;&nbsp;</>
+          )}</>
         </span>
       ))}
     </div>
   )}</>);
 }
-
-/**
- *
- * @param outlineData {[OutlineData]}
- * @param parentId {number}
- */
-function buildBreadcrumbs(outlineData, parentId) {
-  const breadcrumbs = [];
-  if (outlineData && parentId) {
-    for (let i = outlineData.length - 1; i >= 0; i--) {
-      if (outlineData[i].PageID === parentId) {
-        breadcrumbs.push(outlineData[i]);
-        parentId = outlineData[i].ParentID;
-      }
-    }
-  }
-  return breadcrumbs.sort((a, b) => {
-    return b?.OutlineSeq - a?.OutlineSeq
-  });
-}
-
