6bce080f78749d7a49d556a37993a8bf74ba6bda
h2roverpete <pete@h2rover.com>
2025-11-26 20:19:25 -0800

    Unused import.

--- ui/content/Site.js
+++ ui/content/Site.js
@@ -1,7 +1,7 @@
 import {createContext, useEffect, useState} from 'react';
 import ReactGA from 'react-ga4';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Route, Routes, useLocation} from "react-router";
+import {BrowserRouter, Route, Routes} from "react-router";
 
 export const SiteContext = createContext({
   restApi: null,
