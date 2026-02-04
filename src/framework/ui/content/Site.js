5a063e099a2a6798989f88ae32b2b0408ceda9ee
h2roverpete <pete@h2rover.com>
2026-02-04 14:07:10 -0800

    Remove unused import.

--- src/framework/ui/content/Site.js
+++ src/framework/ui/content/Site.js
@@ -1,4 +1,4 @@
-import {createContext, useCallback, useContext, useEffect, useMemo, useState} from 'react';
+import {createContext, useCallback, useContext, useEffect, useState} from 'react';
 import ReactGA from 'react-ga4';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
